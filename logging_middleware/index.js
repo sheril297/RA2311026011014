40b5caebc3b07dac8ca3f3026963b4ec6d72126d
Sheril T Mario <sherilmario9@gmail.com>
2026-05-02 11:09:18 +0530

    Added logging middleware with env setup

--- logging_middleware/index.js
+++ logging_middleware/index.js
@@ -1,1 +1,29 @@
+const axios = require("axios");
+require("dotenv").config();
+const LOG_API = "http://20.207.122.201/evaluation-service/logs";
+const TOKEN = process.env.TOKEN;
+const Log = async (stack, level, packageName, message) => {
+  try {
+    const response = await axios.post(
+      LOG_API,
+      {
+        stack,
+        level,
+        package: packageName,
+        message,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
+    console.log("Log sent:", response.data);
+  } catch (error) {
+    console.error("Logging failed:", error.response?.data || error.message);
+  }
+};
+
+module.exports = Log;
